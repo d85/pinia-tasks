271724a231985ced165e3a2bd3a90c5f50daae82
Dan Carlier <dan.carlier@gmail.com>
2023-03-28 12:05:55 +1100

    Add filters for all tasks and favourite tasks

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,22 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -17,6 +30,7 @@
 </template>
 
 <script>
+  import { ref } from 'vue'
   import TaskDetails from '@/components/TaskDetails.vue'
   import { useTaskStore } from '@/stores/TaskStore'
 
@@ -25,7 +39,9 @@
     setup() {
       const taskStore = useTaskStore()
 
-      return { taskStore }
+      const filter = ref('all')
+
+      return { taskStore, filter }
     }
   }
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,5 +6,19 @@ export const useTaskStore = defineStore('taskStore', {
       {id: 1, title: "buy some milk", isFav: false},
       {id: 2, title: "make some tea", isFav: true},
     ]
-  })
+  }),
+  getters: {
+    favs() {
+      return this.tasks.filter(t => t.isFav)
+    },
+    favCount() {
+      return this.tasks.reduce((acc, cur) => {
+        return cur.isFav ? acc + 1 : acc
+      }, 0)
+    },
+    totalCount: (state) => {
+      // need to pass state if using arrow function
+      return state.tasks.length
+    }
+  }
 })
