404474a1b687df53e292bf2f395a9c05bce5dcd5
Dan Carlier <dan.carlier@gmail.com>
2023-03-29 18:54:16 +1100

    Add json-server and load tasks from db.json

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,9 @@
       <button @click="filter = 'favs'">Fav tasks</button>
     </nav>
 
+    <!-- loading -->
+    <div clas="loading" v-if="taskStore.loading">Loading tasks...</div>
+
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
       <p>You have {{ taskStore.totalCount }} tasks left to do</p>
@@ -44,6 +47,9 @@
     components: { TaskDetails, TaskForm },
     setup() {
       const taskStore = useTaskStore()
+
+      // fetch tasks
+      taskStore.getTasks()
 
       const filter = ref('all')
 
